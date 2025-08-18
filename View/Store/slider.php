f6be65b3fc4cb76ed86b4988ef3bf84494439354
nhatthai <nhatthai9764@gmail.com>
2025-08-18 10:47:21 +0700

    Update code

--- View/Store/slider.php
+++ View/Store/slider.php
@@ -18,29 +18,29 @@
 						<div class="item active">
 							<div class="col-sm-6">
 								<h1><span>THAI</span>-STORE</h1>
-								<h2>Black Jeans with White Shoes</h2>
-								<p>Được thiết kế và cung cấp cho cộng đồng bởi TheBestKing, người sáng tạo ra Fresh Era Clothing. Hãy luôn tươi mới và đừng quên xem các mặt hàng khác do TheBestKing sản xuất trong danh mục.</p>
+								<h2>iPhone 16 Pro Max – Siêu phẩm công nghệ 2025</h2>
+								<p>Được trang bị chip A18 Bionic mạnh mẽ, camera đột phá và thiết kế sang trọng, iPhone 16 Pro Max mang đến trải nghiệm đỉnh cao về hiệu năng, hình ảnh và thời lượng pin. Khám phá ngay tại THAI-STORE để sở hữu sản phẩm công nghệ hàng đầu.</p>
 							</div>
 							<div class="col-sm-6">
-								<img src="https://tr.rbxcdn.com/180DAY-867e46408bf8a81dbf4918d4f3d8c355/420/420/Pants/Webp/noFilter" class="girl img-responsive" alt="" />
+								<img src="https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-16-pro-max.png" class="girl img-responsive" alt="" />
 							</div>
 						</div>
 						<div class="item">
 							<div class="col-sm-6">
 								<h1><span>THAI</span>-STORE</h1>
-								<h2>Down to Earth Hair</h2>
-								<p>Your thoughts are as down to earth as your hair!</p>
+								<h2>Samsung Galaxy S25 Ultra – Sức mạnh của trí tuệ nhân tạo</h2>
+								<p>Trang bị công nghệ Galaxy AI hiện đại, camera siêu zoom sắc nét và hiệu năng vượt trội, Galaxy S25 Ultra mang đến trải nghiệm thông minh, mượt mà và đẳng cấp cho mọi nhu cầu. Khám phá ngay tại THAI-STORE để sở hữu siêu phẩm này.</p>
 							</div>
 							<div class="col-sm-6">
-								<img src="https://tr.rbxcdn.com/180DAY-830dd9cb6cf775bf441a2263b9c91b8b/420/420/Hat/Webp/noFilter" class="girl img-responsive" alt="" />
+								<img src="https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-samsung-galaxy-s25-ultra_3__3.png	" class="girl img-responsive" alt="" />
 							</div>
 						</div>
 
 						<div class="item">
 							<div class="col-sm-6">
 								<h1><span>THAI</span>-STORE</h1>
-								<h2>Knights of Redcliff: Paladin - Face</h2>
-								<p> </p>
+								<h2>Vivo V50 Lite 5G – Siêu pin bền bỉ, hiệu năng vượt trội</h2>
+								<p>Trang bị viên pin dung lượng lớn, hỗ trợ 5G tốc độ cao và bộ nhớ rộng rãi, Vivo V50 Lite 5G mang đến trải nghiệm mượt mà, bền bỉ cho cả công việc lẫn giải trí.</p>
 							</div>
 							<div class="col-sm-6">
 								<img src="https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/v/i/vivo-v50-lite-5g.1.png" class="girl img-responsive" alt="" />
